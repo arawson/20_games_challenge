db3a9fe09f2aa20b2ca06e0df6ff7775175c3580
Aaron Rawson <aaronmrawson@gmail.com>
2023-01-12 08:09:59 -0500

    update with sample ideas

--- W052_SimpleGhostPlatformer/entities/player2/states/in_air/jump.gd
+++ W052_SimpleGhostPlatformer/entities/player2/states/in_air/jump.gd
@@ -1,21 +1,4 @@
-extends "../motion.gd"
-
-export(float) var base_max_horizontal_speed = 400.0
-
-export(float) var air_acceleration = 1000.0
-export(float) var air_deceleration = 2000.0
-export(float) var air_steering_power = 50.0
-
-export(float) var gravity = 1600.0
-
-var enter_velocity = Vector2()
-
-var max_horizontal_speed = 0.0
-var horizontal_speed = 0.0
-var horizontal_velocity = Vector2()
-
-var vertical_speed = 0.0
-var height = 0.0
+extends "../in_air.gd"
 
 func initialize(speed, velocity):
 	horizontal_speed = speed
