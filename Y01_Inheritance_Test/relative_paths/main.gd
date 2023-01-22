7fc571b30017de278b0adc1f25af84b0741cee8f
Aaron Rawson <aaronmrawson@gmail.com>
2023-01-22 16:46:57 -0500

    add test of get_children

---
+++ Y01_Inheritance_Test/relative_paths/main.gd
@@ -0,0 +1,19 @@
+class_name BigOles
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for x in get_children():
+		print("wow!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
