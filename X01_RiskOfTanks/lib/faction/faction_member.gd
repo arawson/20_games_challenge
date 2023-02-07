64f935785aa80d6ec024fe5bd662f0ffb6c61150
Aaron Rawson <aaronmrawson@gmail.com>
2023-02-06 21:19:08 -0500

    add missing signal

--- X01_RiskOfTanks/lib/faction/faction_member.gd
+++ X01_RiskOfTanks/lib/faction/faction_member.gd
@@ -6,14 +6,17 @@ export(int) var faction_id = 0
 export(Resource) var unit_stats_base
 onready var unit_stats: UnitStats = unit_stats_base
 
+var health_max: float
 var health: float
+signal health_lost(old_value, new_value)
 
 # Returns if the damage would kill the unit
 func do_damage(damage: float) -> bool:
-	health -= damage
-	if health < 0:
+	var starting_health = health
+	health = clamp(health - damage, 0, health_max)
+	emit_signal("health_lost", starting_health, health)
+	if health <= 0:
 		health = 0
-		print("%s died" % self)
 		return true
 	return false
 
@@ -22,3 +25,4 @@ func _ready():
 	assert(unit_stats != null)
 	assert(faction_id != 0)
 	health = unit_stats.base_health
+	health_max = unit_stats.base_health
