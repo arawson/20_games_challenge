6be2f7f83373e556f9b3e68013d3fd3ebce4bb80
Aaron Rawson <aaronmrawson@gmail.com>
2024-05-15 18:18:16 -0400

    fix north south sticking correct file

--- 02_exploding_cursors/lib/util.gd
+++ 02_exploding_cursors/lib/util.gd
@@ -12,11 +12,11 @@ func delete_all_children(node: Node):
 func displacement(dir: Direction) -> Vector2:
 	match dir:
 		Direction.NORTH:
-			return Vector2(0, 48)
+			return Vector2(0, -48)
 		Direction.EAST:
 			return Vector2(48, 0)
 		Direction.SOUTH:
-			return Vector2(0, -48)
+			return Vector2(0, 48)
 		Direction.WEST:
 			return Vector2(-48, 0)
 
