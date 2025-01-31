fc8547d2c955b0a07e060bd449f325f7ffb1fda5
Aaron Rawson <aaronmrawson@gmail.com>
2025-01-31 07:51:44 -0500

    i need to write my own scene list manager

---
+++ 04_rpg_testbed/scenes/game_scene/rooms/room.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+signal room_exited(destination: String)
+
+
+@export var room_name: String
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
